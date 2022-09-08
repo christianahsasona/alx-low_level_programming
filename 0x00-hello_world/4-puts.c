eee11bec1ddbac51125e4ffb9a000fac2db809cd
christianahsasona <sasonachristianah@gmail.com>
2022-09-08 21:30:15 +0000

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase "Programming is like a multilingual puzzle
+ * return 0 if exited properly
+ */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-3. script that compiles a C file and creates an executable named cisfun.
+4.C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.

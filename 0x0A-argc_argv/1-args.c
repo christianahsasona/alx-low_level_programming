b684495be6127d802917adb7a0d088b5d8377139
christianahsasona <sasonachristianah@gmail.com>
2022-10-01 19:45:40 +0000

    Program that prints the number of arguments oassde into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * main - Prints the number of argument passed into it
+ * @argc: number of arguments
+ * @argv: arrays of argument
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("argc= %d\n", argc - 1);
+	return (0);
+}

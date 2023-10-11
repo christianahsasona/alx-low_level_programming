766816e3ee75b6680a75e88c3dce34d35af57643
christianahsasona <sasonachristianah@gmail.com>
2023-10-11 00:44:38 +0000

    Hash function

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
-* hash_djb2 -Hash function that implements the djb2 algorithm
-* @str: The string to the hash
-
-* Return: the calculated Hash
-*/
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ *
+ * Return: The calculated hash.
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
